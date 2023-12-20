7b8ef1b66955332bf99f0ba0832bf4efaf75dc4e
pert6000 <pertkoduvere@gmail.com>
2023-12-20 22:41:37 +0200

    jou mees

--- sudoku.py
+++ sudoku.py
@@ -142,6 +142,9 @@ kasutaja_laud = copy.deepcopy(sudoku_laud)
 
 def reeglikontroll(nr, grid, rida, veerg):
     
+    if nr == 0:
+        return True
+    
     read = list(range(len(grid)))
     read.pop(rida)
     #kas samas veerus on
@@ -181,6 +184,19 @@ VALGE = (255, 255, 255)
 MUST = (0, 0, 0)
 SININE = (43, 140, 200)
 LILLA_PUNANE = (153, 1, 71)
+HELEPUNANE = (253, 137, 137)
+
+
+
+font3 = pygame.font.Font("freesansbold.ttf", 32)
+tekst1 = font3.render("Menüü", True, MUST)
+tekst2 = font3.render("Start", True, MUST)
+tekst3 = font3.render("Quit", True, MUST)
+
+
+color_light = (170, 170, 170)
+color_dark = (100, 100, 100)
+
 
 # init
 ekraan = pygame.display.set_mode((LAIUS, KÕRGUS))
@@ -221,82 +237,146 @@ def joonista_valitud_ruut(valitud_ruut):
     i, j = valitud_ruut
     pygame.draw.rect(ekraan, LILLA_PUNANE, (j * RUUDU_SUURUS, i * RUUDU_SUURUS, RUUDU_SUURUS, RUUDU_SUURUS), 3)
 
-kell = pygame.time.Clock()
-valitud_ruut = None
-sisend = 0
 def kirjuta_tühja(valitud_ruut, sisend):
-    if sisend != 0:
+    if sisend != -1:
         i = valitud_ruut[0]
         j = valitud_ruut[1]
         #font = pygame.font.SysFont('blackadderitc', 38, bold=False, italic=False)
         #if usr_laud[i][j]:
-        kasutaja_laud[i][j] = sisend
+        if sudoku_laud[i][j] == 0:
+            kasutaja_laud[i][j] = sisend
             #arv = font.render(str(sisend), True, MUST)
             #x = j * RUUDU_SUURUS + RUUDU_SUURUS // 2 - arv.get_width() // 2
             #y = i * RUUDU_SUURUS + RUUDU_SUURUS // 2 - arv.get_height() // 2
             #ekraan.blit(arv, (x, y))
         #print(usr_laud)
-        sisend = 0
+        sisend = -1
 #ekraan.fill(VALGE)
-while True:
-    vajutus = False
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            hiireX, hiireY = pygame.mouse.get_pos()
-            valitud_ruut = (hiireY // RUUDU_SUURUS, hiireX // RUUDU_SUURUS)
-            print(valitud_ruut)
-        if event.type == pygame.KEYDOWN:
-            #if event.key == pygame.K_0 or pygame.K_KP0:
-                #sisend = 0
-            if event.key == pygame.K_1:
-                vajutus = True
-                sisend = 1
-                print('Vajutati 1')
-            elif event.key == pygame.K_2:
-                vajutus = True
-                sisend = 2
-                print('Vajutati 2')
-            elif event.key == pygame.K_3:
-                vajutus = True
-                sisend = 3
-                print('Vajutati 3')
-            elif event.key == pygame.K_4:
-                vajutus = True
-                sisend = 4
-                print('Vajutati 4')
-            elif event.key == pygame.K_5:
-                vajutus = True
-                sisend = 5
-                print('Vajutati 5')
-            elif event.key == pygame.K_6:
-                vajutus = True
-                sisend = 6
-                print('Vajutati 6')
-            elif event.key == pygame.K_7:
-                vajutus = True
-                sisend = 7
-                print('Vajutati 7')
-            elif event.key == pygame.K_8:
-                vajutus = True
-                sisend = 8
-                print('Vajutati 8')
-            elif event.key == pygame.K_9:
-                vajutus = True
-                sisend = 9
-                print('Vajutati 9')
 
-    ekraan.fill(VALGE)
-    joonista_ruudustik()
-    joonista_arvud()
-    joonista_3ruudu_suurus()
-    if valitud_ruut:
-        joonista_valitud_ruut(valitud_ruut)
-        if vajutus:
-            kirjuta_tühja(valitud_ruut, sisend)
 
-    pygame.display.flip()
-    kell.tick(3)
 
+def main():
+    
+    elusid = 3
+    
+    vead = []
+    kell = pygame.time.Clock()
+    valitud_ruut = None
+    sisend = -1
+    while True:
+        vajutus = False
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                hiireX, hiireY = pygame.mouse.get_pos()
+                valitud_ruut = (hiireY // RUUDU_SUURUS, hiireX // RUUDU_SUURUS)
+                print(valitud_ruut)
+            if event.type == pygame.KEYDOWN:
+                #if event.key == pygame.K_0 or pygame.K_KP0:
+                    #sisend = 0
+                if event.key == pygame.K_1:
+                    vajutus = True
+                    sisend = 1
+                    print('Vajutati 1')
+                elif event.key == pygame.K_2:
+                    vajutus = True
+                    sisend = 2
+                    print('Vajutati 2')
+                elif event.key == pygame.K_3:
+                    vajutus = True
+                    sisend = 3
+                    print('Vajutati 3')
+                elif event.key == pygame.K_4:
+                    vajutus = True
+                    sisend = 4
+                    print('Vajutati 4')
+                elif event.key == pygame.K_5:
+                    vajutus = True
+                    sisend = 5
+                    print('Vajutati 5')
+                elif event.key == pygame.K_6:
+                    vajutus = True
+                    sisend = 6
+                    print('Vajutati 6')
+                elif event.key == pygame.K_7:
+                    vajutus = True
+                    sisend = 7
+                    print('Vajutati 7')
+                elif event.key == pygame.K_8:
+                    vajutus = True
+                    sisend = 8
+                    print('Vajutati 8')
+                elif event.key == pygame.K_9:
+                    vajutus = True
+                    sisend = 9
+                    print('Vajutati 9')
+                elif event.key == pygame.K_BACKSPACE:
+                    vajutus = True
+                    sisend = 0
+
+        ekraan.fill(VALGE)
+        
+        for i in range(len(vead)):
+            pygame.draw.rect(ekraan, HELEPUNANE, (vead[i][1]*RUUDU_SUURUS,vead[i][0]*RUUDU_SUURUS,RUUDU_SUURUS,RUUDU_SUURUS))
+
+        joonista_ruudustik()
+        joonista_arvud()
+        joonista_3ruudu_suurus()
+        if valitud_ruut:
+            joonista_valitud_ruut(valitud_ruut)
+            if vajutus:
+                kirjuta_tühja(valitud_ruut, sisend)
+                if not reeglikontroll(kasutaja_laud[valitud_ruut[0]][valitud_ruut[1]],kasutaja_laud, valitud_ruut[0], valitud_ruut[1]):
+                    vead.append((valitud_ruut[0],valitud_ruut[1]))
+                    elusid -=1
+                print(vead)
+                if (valitud_ruut[0], valitud_ruut[1]) in vead and kasutaja_laud[valitud_ruut[0]][valitud_ruut[1]] == 0:
+                    vead.remove((valitud_ruut[0],valitud_ruut[1]))
+                    
+        pygame.display.flip()
+        kell.tick(60)
+
+
+
+def meny():             # genereerib menüü, kui mängu käima paned
+    kell1 = pygame.time.Clock()
+    while True:
+        kell1.tick(60)
+        mouse = pygame.mouse.get_pos()
+
+        ekraan.fill((255, 255, 255))
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if LAIUS/2 - 40 <= mouse[0] <= LAIUS/2 + 40 and KÕRGUS/2 - 15 <= mouse[1] <= KÕRGUS/2 + 15:
+                    main()
+                elif 410 <= mouse[0] <= 490 and 335 <= mouse[1] <= 365:
+                    pygame.quit()
+
+        # muudab värvi kui hiir "start nupu peal
+        if 405 <= mouse[0] <= 490 and 282 <= mouse[1] <= 317:
+            pygame.draw.rect(ekraan, color_light, [405, 282, 85, 36], 18, 3)
+        else:
+            pygame.draw.rect(ekraan, color_dark, [405, 282, 85, 36], 18, 3)
+
+        # muudab värvi kui hiir "quit" nupu peal
+        if 405 <= mouse[0] <= 490 and 332 <= mouse[1] <= 367:
+            pygame.draw.rect(ekraan, color_light, [405, 332, 85, 36], 18, 3)
+        else:
+            pygame.draw.rect(ekraan, color_dark, [405, 332, 85, 36], 18, 3)
+
+        # joonistab tekstid menüüle
+        ekraan.blit(tekst1, (10, 10))
+        ekraan.blit(tekst2, (410, 285))
+        ekraan.blit(tekst3, (412, 335))
+
+        pygame.display.update()
+
+
+
+# meny()
+main()
